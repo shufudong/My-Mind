4e4c3d982388a07edd28b60766cec7ef919eba08
shufudong <shufudong@gmail.com>
2019-01-31 00:07:15 +0800

    no message

--- JavaScript高级程序设计/Part-2-Using-JavaScript-in-HTML/test.js
+++ JavaScript高级程序设计/Part-2-Using-JavaScript-in-HTML/test.js
@@ -1,25 +1,10 @@
-function createPerson(name, age, job) {
+function Person(name,age,obj){
     var o = new Object();
     o.name = name;
     o.age = age;
     o.job = job;
-    o.sayName = function () {
+    o.sayName = function(){
         alert(this.name);
     };
     return o;
 }
-
-var person1 = createPerson("Nicholas", 29, "Software Engineer");
-var person2 = createPerson("Greg", 27, "Doctor");
-
-function Person(name, age, job) {
-    this.name = name;
-    this.age = age;
-    this.job = job;
-    this.sayName = function () {
-        alert(this.name);
-    };
-}
-
-var person1 = new Person("Nicholas", 29, "Software Engineer");
-var perosn2 = new Person("Greg", 27, "Doctor");
